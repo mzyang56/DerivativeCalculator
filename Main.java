f7b243be48bec4dc50e38ca8f8ac396525df22a9
mzyang56 <myang07@ladueschools.net>
2023-05-08 15:29:32 +0000

    Max Yang Andrew Stappenbeck

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-//Max Yang B1
-//Recursion Calculator Project
-//04.03.23
+//Max Yang and Andrew Stappenbeck B1
+//Derivative Calculator Project
+//05.08.23
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,23 +10,24 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter an expression with only positive integers, without spaces, and only using these symbols:  +, -, *, /, ^, (, ) ");
+
+        //Clears screen
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+        System.out.println("Please enter an expression with positive integers, '+', '-', 'x' and/or parantheses (for chain rule), without any spaces.");
+        System.out.println("\nValid expressions include:\n   x^10 \n   3x^6 \n   x^3+2x^2+12x+6\n   3(15x^6+3)^3\n");
         String input = sc.nextLine();
         
         //Creates an ArrayList that contains all of the numbers and modifiers
         List<String> eq1 = new ArrayList<String>();
         List<String> eq2 = new ArrayList<String>();
         
-        /* List<String> equation3 = new ArrayList<String>();
-                equation3 = equation.subList(parPos+1, endParPos);
-                System.out.println("Equation3 " + equation3);
-                */
-        
-        
+        //Variable that when true creates a second array that will not be differentiated due to the chain rule
         boolean innerList = false;
+
         for (int i = 0; i < input.length(); i++) {
             
-            //Checks if input contains only integers and symbols
+            //Checks if input contains only integers and valid symbols
             if (isValid(input.substring(i,i+1)) == false) {
                 System.out.println("Syntax Error: Please enter a valid input");
                 System.exit(0);
@@ -38,9 +39,21 @@ public class Main {
                 if (innerList == true) {
                     eq2.add("x");
                 }
+
+                try {
+                    //If x is not preceded by a coefficient, this adds a "1"
+                    if (isInt(input.substring(i-1,i)) == false) {
+                        eq1.add(i, "1");
+                    }
+                    
+                } catch (Exception k) {
+                    eq1.add(i, "1");
+                    
+                }
             }
 
             else if (input.substring(i,i+1).equals("(")) {
+                //Brackets signify an equation that needs to be differentiated with the chain rule
                 eq1.add("[");
                 eq2.add("(");
                 innerList = true;
@@ -68,7 +81,8 @@ public class Main {
                 
                 //Finds integers that do not accompany a variable
                 try {
-                    if (input.substring(i-1, i+n).equals("^") || input.substring(i+1, i+n+2).equals("^") || input.substring(i+1, i+n+2).equals("x")) {
+                    //Checks if the number is a coefficient for x or is an exponent
+                    if (input.substring(i-1, i).equals("^") || input.substring(i+n+1, i+n+2).equals("^") || input.substring(i+n+1, i+n+2).equals("x") || input.substring(i+n+1, i+n+2).equals("(") ) {
                         eq1.add(input.substring(i, i+n+1));
                         if (innerList == true) {
                             eq2.add(input.substring(i, i+n+1));
@@ -76,7 +90,9 @@ public class Main {
                     }
                     
                     else {
+                        //Adds a "del" otherwise, which will be removed once differentiated
                         eq1.add("del");
+                        //However, if the equation involves Chain Rule, the number will be added to the eq2 ArrayList which represents the undifferentiated part of the Chain Rule
                         if (innerList == true) {
                             eq2.add(input.substring(i, i+n+1));
                         }
@@ -87,7 +103,7 @@ public class Main {
                 catch (Exception e) {
                     
                     try {
-                        if (input.substring(i+1, i+n+2).equals("^") || input.substring(i+1, i+n+2).equals("x")) {
+                        if (input.substring(i+n+1, i+n+2).equals("x") || input.substring(i+n+1, i+n+2).equals("(")) {
                             eq1.add(input.substring(i, i+n+1));
                             if (innerList == true) {
                                 eq2.add(input.substring(i, i+n+1));
@@ -110,6 +126,7 @@ public class Main {
 
                 } 
 
+                //Increments i by an additional int n so integers with several digits are properly separated
                 i += n;
             }
             
@@ -126,11 +143,8 @@ public class Main {
     public static List<String> calculate(List<String> equation, List<String> equation2) {
 
         while (equation.contains("o")) {
-            
-            System.out.println(equation);
 
-            int total = 0;
-            //Checks for statements in parantheses and then performs recursion to calculate value
+            //Checks for statements in parantheses and then performs recursion to calculate value using manual Chain Rule
             if (equation.contains("[")) {
                 int parCounter = 0;
                 int parPos = equation.indexOf("[");
@@ -161,18 +175,20 @@ public class Main {
 
                 }
 
-                //Chain Rule calculation
                 if (!equation.get(endParPos+1).equals("^")) {
                     equation.remove(endParPos);
                 }
                 
                 else {
                     int exponent = Integer.parseInt(equation.get(endParPos+2));
-                    
                     int coef = 1;
+
+                    int digitsAdded = 0;
                     try {
                         if (isInt(equation.get(parPos-1)) == true) {
                             coef = Integer.parseInt(equation.get(parPos-1));
+                            //Variable used to account for the absence of an index shift when a coefficient is already present for Chain Rule
+                            digitsAdded = 1;
                         }
 
                         else {
@@ -185,66 +201,35 @@ public class Main {
                         equation.add(parPos, "1");
                     }
 
-                    //performs calculation
-                    equation.set(parPos, Integer.toString(coef * exponent));
+                    //Calculates coefficient
+                    equation.set(parPos-digitsAdded, Integer.toString(coef * exponent));
                     
                     if (exponent != 1) {
-                        System.out.println("Now " + equation);
-                        equation.set(endParPos+3, Integer.toString(exponent-1));
-                        equation.set(parPos+1, "(");
-                        equation.set(endParPos+1, ")");
+                        equation.set(endParPos+3-digitsAdded, Integer.toString(exponent-1));
+                        //Converts "[" and "]" to "(" and ")" to indicate that the chain rule differentiation has been applied
+                        equation.set(parPos+1-digitsAdded, "(");
+                        equation.set(endParPos+1-digitsAdded, ")");
 
-                        List<String> chnRule = equation.subList(parPos+1, endParPos+2);
-                        int oPos = chnRule.indexOf("o");
-                        System.out.println("Equation 2 " + equation2);
-                        
+                        List<String> chnRule = equation.subList(parPos+1-digitsAdded, endParPos+2);
                         int size = chnRule.size();
                         
-                        /* 
-                        for (int i = endParPos+1; i > parPos; i--) {
-                            equation.remove(i);
-                        }
-                        */
+                        //Merges the undifferentiated equation2 with the rest of the equation
+                        equation.addAll(parPos+1+size-2*digitsAdded, equation2);
 
-                        
-                        equation.addAll(parPos+1+size, equation2);
-                        //equation.set(oPos+size+1, "x");
-                        
-                        //equation.addAll(parPos+1, chnRule);
-                        //System.out.println("chnRule " + chnRule);
-                        
-                        
-                        System.out.println("Equation All 2 " + equation);
-                        
-                        
-                        System.out.println("Here " + equation.subList(parPos+1, parPos+size));
-                        
-                        System.out.println(equation);
-                        //System.out.println(equation.subList(parPos+2+chnRule.size(), parPos+2+2*chnRule.size()));
-
-                        calculate(equation.subList(parPos+2+size, parPos+2*size), equation2);
+                        //Performs recursion for the rest of the equation that needs to be differentiated due to the Chain Rule
+                        calculate(equation.subList(parPos+2+size-digitsAdded, parPos+2*size-digitsAdded), equation2);
 
                     }
-                    else {
-                        //equation.set(xPos, "x");
-                        //equation.remove(xPos+2);
-                        //equation.remove(xPos+1);
-                    }
-                    
-                    //equation.set(xPos, "x");
-                }
-
-                //equation.remove(endParPos);
-                
+                } 
             }
 
-            //Checks for statements with "o" (placeholder for x) to derive
+            //Checks for statements with "o" (placeholder for x) to determine where to derive
             if (equation.contains("o")) {
                 //finds index
                 int xPos = equation.indexOf("o");
                 int coef = 0;
                 
-                
+                //Checks if there is an exponent sign behind the x
                 try {
                     if (!equation.get(xPos+1).equals("^")) {
                         //Removes x if there is no exponent, ex: 12x -> 12
@@ -252,29 +237,35 @@ public class Main {
                     }
                     
                     else {
+                        //Stores the exponent value
                         int exponent = Integer.parseInt(equation.get(xPos+2));
                         try {
+                            //Checks if there is a coefficient in front of the x
                             if (isInt(equation.get(xPos-1)) == true) {
                                 coef = Integer.parseInt(equation.get(xPos-1));
                             }
                         } 
                         catch (Exception e) {
+                            //Runs if there is no coefficient, in which case the coefficient is 1
                             coef = 1;
                             equation.set(xPos, "1");
                         }
 
-                        //performs calculation
+                        //Calculates the new coefficient
                         equation.set(xPos-1, Integer.toString(coef * exponent));
                         
                         if (exponent != 1) {
+                            //Manually performs power rule
                             equation.set(xPos+2, Integer.toString(exponent-1));
                         }
                         else {
+                            //Converts "o" to "x" so program knows differentiation has been completed
                             equation.set(xPos, "x");
                             equation.remove(xPos+2);
                             equation.remove(xPos+1);
                         }
                         
+                        //Converts "o" to "x" so program knows differentiation has been completed
                         equation.set(xPos, "x");
                     }
                 }
@@ -283,22 +274,22 @@ public class Main {
                     equation.remove(xPos);
                 }
 
-                System.out.println(equation);
-
             }
             
         }
 
+        //Removes integers that are not coefficients or exponents that are already marked as "del"
         while (equation.contains("del")) {
             int delPos = equation.indexOf("del");
             equation.remove(delPos);
 
+            //Removes any "+" or "-" operators if present
             try {
                 equation.remove(delPos-1);
-            } catch (Exception h) {
+            } 
+            catch (Exception h) {
                 
             }
-            
         }
 
         return equation;
@@ -306,7 +297,7 @@ public class Main {
 
     //Tests if input contains only integers and symbols
     public static boolean isValid(String s) {
-        if (s.equals("(") || s.equals(")") || s.equals("x") || s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("^")) {
+        if (s.equals("(") || s.equals(")") || s.equals("x") || s.equals("+") || s.equals("-") || s.equals("^")) {
             return true;
         }
 
@@ -323,42 +314,14 @@ public class Main {
         }
     }
 
-    //Tests if input is an integer
+    //Method that checks if input is an integer
     public static boolean isInt(String strNum) {
-            if (strNum.equals("x") || strNum.equals("+") || strNum.equals("-") || strNum.equals("*") || strNum.equals("/") || strNum.equals("^") || strNum.equals("(") || strNum.equals(")")) {
+            if (strNum.equals("x") || strNum.equals("+") || strNum.equals("-") || strNum.equals("^") || strNum.equals("(") || strNum.equals(")")) {
                 return false;
             }
             else {
                 return true;
             } 
     }
-
-
-    /*
-    //ex: x^2
-    public static String polynomialDerivative(String input, String exponent, int coefficient)
-    {
-      String output;
-      int newCoeff = Integer.parseInt(exponent) * coefficient;
-
-      output = newCoeff + "*" + input;
-
-      int power = Integer.parseInt(exponent);
-      power--;
-      exponent = power + "";
-
-      if (power != 0) {
-        output = output.substring(0, output.indexOf("^")) + "^" +  exponent;
-      }
-      
-      else {
-        output = "1";
-      }
-      
-      return output;
-      
-    }
-     */
-   
 
 }
